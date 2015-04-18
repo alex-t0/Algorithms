2403ef499855c6c0bba306859ba58ab82dc26547
Alex T <qwerty@alex-t.net>
2015-04-19 00:57:10 +0300

    generated methods

--- Algo/src/net/alext/algorithm/divide/and/conquer/MaximumSubarrayAlgorithm.java
+++ Algo/src/net/alext/algorithm/divide/and/conquer/MaximumSubarrayAlgorithm.java
@@ -1,5 +1,25 @@
 package net.alext.algorithm.divide.and.conquer;
 
-public class MaximumSubarrayAlgorithm {
+import java.util.List;
+
+public class MaximumSubarrayAlgorithm<TInput, TResult> extends DivideAndConquerAlgorithm<TInput, TResult> {
+
+	@Override
+	public TResult ProcessSimple(TInput simple) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TInput> Divide(TInput input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TResult Conquer(List<TResult> simples) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

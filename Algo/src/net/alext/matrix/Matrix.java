38818bb9b44b3434dcc425b3dc424825a64fffbd
Alex T <qwerty@alex-t.net>
2015-05-09 01:33:43 +0300

    addition

--- Algo/src/net/alext/matrix/Matrix.java
+++ Algo/src/net/alext/matrix/Matrix.java
@@ -19,12 +19,12 @@ public class Matrix<TValue extends BaseBox<T>, T extends Comparable<T>> {
 		return matrix[0].length;
 	}
 	
-	public T get(int x, int y){
-		return matrix[x][y].unbox();
+	public TValue get(int x, int y){
+		return matrix[x][y];
 	}
 	
-	public void set(T val, int x, int y){
-		matrix[x][y].unbox();
+	public void set(TValue val, int x, int y){
+		matrix[x][y] = val;
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -39,18 +39,36 @@ public class Matrix<TValue extends BaseBox<T>, T extends Comparable<T>> {
 			return false;
 		
 		for (int i = 0; i < getX(); i++)
-			for (int j = 0; i < getY(); j++)
+			for (int j = 0; i < getY(); j++){
+				if (get(i, j) == null || otherMatrix.get(i, j) == null)
+					return false;
+				
 				if (!get(i, j).equals(otherMatrix.get(i, j)))
 					return false;
-		
+			}
+				
 		return true;
 	}
 	
-	public Matrix<TValue, T> add(Matrix<TValue, T> other) {
+	@SuppressWarnings("unchecked")
+	public Matrix<TValue, T> add(Matrix<TValue, T> other) throws CloneNotSupportedException {
 		if (getX() != other.getX() || getY() != other.getY())
 			throw new IllegalStateException("Size must be identical to add");
 		
 		Matrix<TValue, T> result = new Matrix<TValue, T>(getX(), getY());
+		
+		for (int i = 0; i < getX(); i++)
+			for (int j = 0; j < getY(); j++)
+			{
+				if (get(i, j) == null || other.get(i, j) == null)
+					throw new IllegalStateException("Size must be identical to add");
+				
+				TValue sum = (TValue) get(i, j).clone();
+				
+				sum.box(get(i, j).add(other.get(i, j)).unbox());
+				
+				result.set(sum, i, j);
+			}
 		
 		return result;
 	}

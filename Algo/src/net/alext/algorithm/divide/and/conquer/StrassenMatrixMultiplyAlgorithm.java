7bbd69a86a7a068c91ea904239f3d96983a4aad6
Alex T <qwerty@alex-t.net>
2015-05-21 23:19:30 +0300

    stub

--- Algo/src/net/alext/algorithm/divide/and/conquer/StrassenMatrixMultiplyAlgorithm.java
+++ Algo/src/net/alext/algorithm/divide/and/conquer/StrassenMatrixMultiplyAlgorithm.java
@@ -7,28 +7,30 @@ import net.alext.boxing.BaseBox;
 import net.alext.matrix.Matrix;
 
 public class StrassenMatrixMultiplyAlgorithm<TValue extends BaseBox<T>, T> 
-	extends DivideAndConquerAlgorithm<Matrix<TValue, T>, Matrix<TValue, T>, Matrix<TValue, T>, Matrix<TValue, T>> {
+	extends DivideAndConquerAlgorithm<Matrix<TValue, T>[], Matrix<TValue, T>, Matrix<TValue, T>, Matrix<TValue, T>> {
 
 	@Override
-	protected Matrix<TValue, T> ProcessSimple(Matrix<TValue, T> input,
+	protected Matrix<TValue, T> ProcessSimple(Matrix<TValue, T>[] input,
 			Matrix<TValue, T> simple) throws DivideAndConquerAlgorithmException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected List<Matrix<TValue, T>> Divide(Matrix<TValue, T> input)
+	protected List<Matrix<TValue, T>> Divide(Matrix<TValue, T>[] input)
 			throws DivideAndConquerAlgorithmException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected Matrix<TValue, T> Conquer(Matrix<TValue, T> input,
+	protected Matrix<TValue, T> Conquer(Matrix<TValue, T>[] input,
 			List<Matrix<TValue, T>> simples)
 			throws DivideAndConquerAlgorithmException {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+
 
 }
